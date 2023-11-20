ff621da0b77c135987fdf8ef743f858fee680cdd
sumitFluxbyte <sumitpatel.fluxbyte@gmail.com>
2023-11-20 11:37:21 +0530

    remove api change password form enviroment and done lint fix

--- packages/frontend/src/Environment.ts
+++ packages/frontend/src/Environment.ts
@@ -3,5 +3,4 @@ export const baseURL = "http://192.168.29.102:8000/api";
 export const requestURLs = {
   login: `${baseURL}/auth/login`,
   signup: `${baseURL}/auth/sign-up`,
-  changePassword: `${baseURL}/auth/change-password`,
 };
