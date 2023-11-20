ed13df98c18456d0c0d8d034d28d4fad6fae40ee
sumitFluxbyte <sumitpatel.fluxbyte@gmail.com>
2023-11-20 10:21:40 +0530

    Fix lint issue and remove UserApi.tsx file

--- packages/frontend/.eslintrc.cjs
+++ packages/frontend/.eslintrc.cjs
@@ -2,40 +2,42 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'indent': ['warn', 2],
-    'semi': [
-      'warn', 'always', {
-        omitLastInOneLineClassBody: true
-      }
-    ],
-    'comma-dangle': [
-      'warn',
+    "@typescript-eslint/no-explicit-any": "warn",
+    indent: ["warn", 2],
+    semi: [
+      "warn",
+      "always",
       {
-        'arrays': 'always',
-        'objects': 'always',
-        'imports': 'never',
-        'exports': 'never',
-        'functions': 'never'
-      }
+        omitLastInOneLineClassBody: true,
+      },
     ],
-    'no-duplicate-imports': ['warn'],
-    'no-console': ['warn', { 'allow': ['warn', 'error'] }],
-    'no-eval': ['warn', { 'allowIndirect': true }],
-    'no-debugger': 'error',
-    'require-await': 'warn',
-    'no-promise-executor-return': 'warn'
+    "comma-dangle": [
+      "warn",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "never",
+      },
+    ],
+    "no-duplicate-imports": ["warn"],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-eval": ["warn", { allowIndirect: true }],
+    "no-debugger": "error",
+    "require-await": "warn",
+    "no-promise-executor-return": "warn",
   },
-}
+};
