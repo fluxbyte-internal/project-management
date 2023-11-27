f9fbf4cd69a6f252a521688acf3d4af193d9df31
hitesh3121 <hitesh.fluxbyte@gmail.com>
2023-11-27 19:05:21 +0530

    Nonworking days dropdown -UI changes - package

--- packages/frontend/src/pages/organisation/organisationForm/index.tsx
+++ packages/frontend/src/pages/organisation/organisationForm/index.tsx
@@ -9,6 +9,20 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import useCurrentUserQuery from "@/api/query/useCurrentUserQuery";
 import countries from "../../../assets/json/countries.json";
+import { useState } from "react";
+import {
+  CommandInput,
+  CommandGroup,
+  CommandItem,
+  CommandDialog,
+} from "@/components/ui/command";
+import { cn } from "@/lib/utils";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Check } from "lucide-react";
 interface Props {
   close: () => void;
 }
@@ -22,13 +36,23 @@ function OrganisationForm(props: Props) {
   const navigate = useNavigate();
   const organisationMutation = useOrganisationMutation();
   const { refetch, isFetched } = useCurrentUserQuery();
-
+  const [open, setOpen] = useState(true);
+  const [value, setValue] = useState<string[]>([]);
+  const nonWorkingDays = [
+    { key: "SUN", value: "Sunday" },
+    { key: "MON", value: "Monday" },
+    { key: "TUE", value: "Tuesday" },
+    { key: "WED", value: "Wednesday" },
+    { key: "THU", value: "Thursday" },
+    { key: "FRI", value: "Friday" },
+    { key: "SAT", value: "Saturday" },
+  ];
   const formik = useFormik<z.infer<typeof createOrganisationSchema>>({
     initialValues: {
       organisationName: "",
       industry: "",
       status: "ACTIVE",
-      listOfNonWorkingDays: 1,
+      nonWorkingDays: [],
       country: "",
     },
     validationSchema: toFormikValidationSchema(createOrganisationSchema),
@@ -64,6 +88,19 @@ function OrganisationForm(props: Props) {
     },
   });
 
+  const setValues = (item: string) => {
+    const val = item.toUpperCase();
+    const isItemInArray = value.includes(val);
+    if (isItemInArray) {
+      const newArray = value.filter((i) => i !== val);
+      setValue(newArray);
+      formik.setFieldValue("nonWorkingDays", newArray);
+    } else {
+      const newArray = [...value, val];
+      setValue(newArray);
+      formik.setFieldValue("nonWorkingDays", newArray);
+    }
+  };
   return (
     <div className="absolute w-full h-full z-50 top-full left-full -translate-x-full -translate-y-full flex justify-center items-center bg-gray-900 bg-opacity-50 ">
       <div className="bg-white rounded-lg shadow-md px-2.5 md:px-6 lg:px-8 pt-6 pb-8 mb-4 md:w-3/4 w-11/12 lg:w-[40rem]">
@@ -107,20 +144,44 @@ function OrganisationForm(props: Props) {
               {formik.touched.industry && formik.errors.industry}
             </span>
           </div>
-          <div >
+          <div>
             <label className={labelStyle}>Working Days</label>
-            <input
-              className={inputStyle}
-              name="listOfNonWorkingDays"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.listOfNonWorkingDays}
-              type="number"
-              placeholder="Working Days"
-            />
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={"outline"}
+                  className="w-full justify-start text-gray-400"
+                  onClick={() => setOpen(true)}
+                  aria-expanded={open}
+                >
+                  {value&&value.length ? value.map((i)=>{return(<span className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{i}</span>)}) : "Select framework..."}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0">
+                <CommandDialog open={open} onOpenChange={() => setOpen(false)}>
+                  <CommandInput placeholder="search..."  />
+                  <CommandGroup>
+                    {nonWorkingDays.map((day) => (
+                      <CommandItem
+                        key={day.value}
+                        value={day.key}
+                        onSelect={setValues}
+                      >
+                        <Check
+                          className={cn(
+                            "mr-2 h-4 w-4",
+                            value.includes(day.key)  ? "opacity-100" : "opacity-0"
+                          )}
+                        />
+                        {day.value}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandDialog>
+              </PopoverContent>
+            </Popover>
             <span className={errorStyle}>
-              {formik.touched.listOfNonWorkingDays &&
-                formik.errors.listOfNonWorkingDays}
+              {formik.touched.nonWorkingDays && formik.errors.nonWorkingDays}
             </span>
           </div>
           <div >
@@ -134,9 +195,11 @@ function OrganisationForm(props: Props) {
               placeholder="Country"
             >
               <option value="">Select Country</option>
-              {countries.map((country) => {
+              {countries.map((country, index) => {
                 return (
-                  <option value={country.isoCode}>{country.name}</option>
+                  <option key={index} value={country.isoCode}>
+                    {country.name}
+                  </option>
                 );
               })}
             </select>
