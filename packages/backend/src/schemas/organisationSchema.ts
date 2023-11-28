3ac45ae226b425580c2a926cc67b39ca7dd13a78
hitesh3121 <hitesh.fluxbyte@gmail.com>
2023-11-28 10:44:00 +0530

    Enum set - Organisation status

--- packages/backend/src/schemas/organisationSchema.ts
+++ packages/backend/src/schemas/organisationSchema.ts
@@ -1,3 +1,4 @@
+import { OrgStatusEnum } from "@prisma/client";
 import { z } from "zod";
 
 export enum OrgListOfNonWorkingDaysEnum {
@@ -15,7 +16,7 @@ export const organisationIdSchema = z.string().uuid();
 export const createOrganisationSchema = z.object({
   organisationName: z.string().min(1),
   industry: z.string().min(1),
-  status: z.string().min(1),
+  status: z.nativeEnum(OrgStatusEnum),
   country: z.string().min(1),
   nonWorkingDays: z.nativeEnum(OrgListOfNonWorkingDaysEnum).array()
 });
@@ -23,7 +24,7 @@ export const createOrganisationSchema = z.object({
 export const updateOrganisationSchema = z.object({
   organisationName: z.string().min(1).optional(),
   industry: z.string().min(1).optional(),
-  status: z.string().min(1).optional(),
+  status: z.nativeEnum(OrgStatusEnum).optional(),
   country: z.string().min(1).optional(),
   nonWorkingDays: z.nativeEnum(OrgListOfNonWorkingDaysEnum).array().optional()
 });
