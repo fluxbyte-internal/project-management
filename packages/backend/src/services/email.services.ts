c8fcd89e57dbb4dd4d8f18f0e1fc2b475d6fc88b
hitesh3121 <hitesh.fluxbyte@gmail.com>
2023-11-30 11:03:14 +0530

    remove promises from send email

--- packages/backend/src/services/email.services.ts
+++ packages/backend/src/services/email.services.ts
@@ -7,40 +7,34 @@ export class EmailService {
     subjectMessage: string,
     bodyMessage: string,
   ) {
-    return new Promise<AWS.SES.SendEmailResponse>(async (
-      resolve, reject
-    ) => {
-      AWS.config.update({
-        region: settings.emailCredentials.region,
-        accessKeyId: settings.emailCredentials.accessKeyId,
-        secretAccessKey: settings.emailCredentials.secretAccessKey
-      }
-      );
-      const ses = new AWS.SES();
-
-      const params = {
-        Destination: {
-          ToAddresses: [toEmail],
-        },
-        Message: {
-          Body: {
-            Text: {
-              Data: bodyMessage,
-            },
-          },
-          Subject: {
-            Data: subjectMessage,
-          },
-        },
-        Source: 'yashmadlani710@gmail.com',
-      };
-      try {
-        const result = await ses.sendEmail(params).promise();
-        resolve(result);
-      } catch (error) {
-        console.error(error);
-        reject(error);
-      }
+    AWS.config.update({
+      region: settings.emailCredentials.region,
+      accessKeyId: settings.emailCredentials.accessKeyId,
+      secretAccessKey: settings.emailCredentials.secretAccessKey
     });
+    const ses = new AWS.SES();
+    const params = {
+      Destination: {
+        ToAddresses: [toEmail],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: bodyMessage,
+          },
+        },
+        Subject: {
+          Data: subjectMessage,
+        },
+      },
+      Source: 'yashmadlani710@gmail.com',
+    };
+    try {
+      const result = await ses.sendEmail(params).promise();
+      return result;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   };
 };
