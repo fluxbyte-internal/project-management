17962511d8fe98a6615e41f0e16c47813ea90394
sumitFluxbyte <sumitpatel.fluxbyte@gmail.com>
2023-12-01 14:56:16 +0530

    remove unused import

--- packages/backend/src/routers/user.routes.ts
+++ packages/backend/src/routers/user.routes.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import * as UserController from '../controllers/user.controller.js';
-import { authMiddleware } from '../middleware/auth.middleware.js';
 
 let router = express.Router();
 
