5e8793ebe49bd220a5ceaab5758f3eb5d80254e2
KhushalRawal <93979602+KhushalRawal@users.noreply.github.com>
2023-12-05 17:46:09 +0530

    - Signup/Login UI Completed. (#108)
    - authController, authSchema Updated.
    
    Co-authored-by: Sandesh Ghone <sandesh.fluxbyte@gmail.com>

--- packages/frontend/src/pages/authentication/login/index.tsx
+++ packages/frontend/src/pages/authentication/login/index.tsx
@@ -11,6 +11,9 @@ import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth";
 import { isAxiosError } from "axios";
 import InputEmail from "@/components/common/InputEmail";
+import SignUp from "../../../assets/svg/signup.svg";
+import Google from "../../../assets/svg/google.svg";
+import Facebook from "../../../assets/svg/facebook.svg";
 
 export type LoginValues = {
   email: string;
@@ -61,28 +64,28 @@ function Login() {
   };
 
   return (
-    <>
-      <div className="flex flex-col px-2 items-center min-h-screen py-6 justify-center bg-gradient-to-t from-[#FFF8DF] to-[#FFD6AB]">
-        <div className="w-[min(400px,100%)] space-y-4 py-4 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
+    <div className="flex justify-center min-h-screen">
+      <div className="lg:flex flex-col items-center justify-center lg:w-1/2 bg-gradient-to-t from-[#FFF8DF] to-[#FFD6AB] hidden">
+        <img src={SignUp} />
+      </div>
+      <div className="flex flex-col items-center w-full lg:w-1/2 justify-center overflow-hidden bg-white">
+        <div className="w-full sm:px-14 max-w-xl">
           <div className="px-4">
             <h3 className="text-2xl text-center font-bold text-primary-900">
               Login
             </h3>
           </div>
-          <hr />
+          <hr className="m-4" />
           <form onSubmit={formik.handleSubmit} className="px-4">
-            <div className="mt-1">
+            <div className="w-full mt-1">
               <label htmlFor="email" className={labelStyle}>
                 Email
               </label>
               <InputEmail
                 name="email"
-                id="email"
                 placeholder="Enter email"
                 value={formik.values.email}
-                onChange={(e) => {
-                  formik.handleChange(e);
-                }}
+                onChange={formik.handleChange}
               />
               <div>
                 <ErrorMessage>
@@ -90,25 +93,31 @@ function Login() {
                 </ErrorMessage>
               </div>
             </div>
-            <div className="relative w-full mt-1">
+            <div className="w-full mt-1">
               <label htmlFor="password" className={labelStyle}>
                 Password
               </label>
-              <input
-                type={`${showPassword ? "text" : "password"}`}
-                name="password"
-                className={inputStyle}
-                placeholder="Enter password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-              />
-              <div className="p-2 absolute top-[40%] right-[5%]">
-                <img
-                  src={showPassword ? show : hide}
-                  onClick={handleShowPassword}
-                  width={16}
-                  height={16}
+              <div className="relative mt-1">
+                <input
+                  type={`${showPassword ? "text" : "password"}`}
+                  name="password"
+                  className={inputStyle}
+                  placeholder="Enter password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
                 />
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  onClick={handleShowPassword}
+                  className="absolute top-1/2 right-1 -translate-y-1/2 mt-[1px]"
+                >
+                  <img
+                    src={showPassword ? show : hide}
+                    width={16}
+                    height={16}
+                  />
+                </Button>
               </div>
               <div>
                 <ErrorMessage>
@@ -116,23 +125,17 @@ function Login() {
                 </ErrorMessage>
               </div>
             </div>
-            <div className="flex items-center mt-1.5">
+            <div className="flex items-center">
               <Button
                 type="submit"
                 variant={"primary"}
-                className="w-full"
                 isLoading={isLoading}
                 disabled={isLoading}
+                className="w-full py-2.5 mt-1.5 rounded-md hover:bg-opacity-80 disabled:bg-opacity-50"
               >
                 Submit
               </Button>
             </div>
-            <a
-              href="#"
-              className="mt-4 text-xs text-right text-danger hover:underline"
-            >
-              Forget Password?
-            </a>
             <div className="mt-4 text-grey-600">
               New user?{" "}
               <NavLink className="text-warning hover:underline" to="/signup">
@@ -140,9 +143,40 @@ function Login() {
               </NavLink>
             </div>
           </form>
+          <div className="flex items-center p-4">
+            <div className="w-full pr-2"><hr /></div>
+            <div className="w-full bg-[#E7E7E7] text-xs py-1 rounded-lg text-center">Or continue with</div>
+            <div className="w-full pl-2"><hr /></div>
+          </div>
+          <div className="flex flex-col px-4 gap-4">
+            <Button
+              type="button"
+              variant={"outline"}
+              isLoading={isLoading}
+              disabled={isLoading}
+              className="w-full flex py-2.5 mt-1.5 rounded-md gap-2.5 hover:bg-opacity-80 disabled:bg-opacity-50"
+            >
+              <img src={Google} />
+              <span>
+                Sign in with Google
+              </span>
+            </Button>
+            <Button
+              type="button"
+              variant={"secondary"}
+              isLoading={isLoading}
+              disabled={isLoading}
+              className="w-full flex py-2.5 mt-1.5 rounded-md gap-2.5 hover:bg-[#1876f2d8] bg-[#1877F2] text-white"
+            >
+              <img src={Facebook} />
+              <span>
+                Sign in with Facebook
+              </span>
+            </Button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
