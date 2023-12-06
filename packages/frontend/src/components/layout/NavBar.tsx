cab918a90eafc9e96a358b3886b88aea63ab5740
KhushalRawal <khushalfluxbyte@gmail.com>
2023-12-06 13:08:01 +0530

    UI issue - 1: Resolved. Update code - div tag replaced with common button tag.

--- packages/frontend/src/components/layout/NavBar.tsx
+++ packages/frontend/src/components/layout/NavBar.tsx
@@ -206,15 +206,40 @@ function NavBar() {
           )}
         </div>
         <div className="flex md:gap-5 gap-2 items-center relative">
-          <div className="w-8 h-8 aspect-square rounded-full bg-primary-100 md:block hidden cursor-pointer">
-            <img
-              src={Notification}
-              className="w-full h-full justify-center flex p-1"
-            ></img>
-          </div>
-          <div className="w-8 h-8 aspect-square cursor-pointer md:block hidden">
-            <img src={Information} className="w-full h-full"></img>
-          </div>
+          <Button className="relative w-8 h-8 aspect-square rounded-full bg-transparent active:bg-primary-100 hover:bg-primary-100 md:block hidden cursor-pointer">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute top-1 left-1"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M6.58597 17.829C6.7717 18.0148 6.99221 18.1622 7.23492 18.2628C7.47762 18.3633 7.73776 18.4151 8.00047 18.4151C8.26318 18.4151 8.52332 18.3633 8.76602 18.2628C9.00873 18.1622 9.22924 18.0148 9.41497 17.829L6.58497 15C6.39917 15.1857 6.25178 15.4062 6.15122 15.6489C6.05065 15.8917 5.9989 16.1518 5.9989 16.4145C5.9989 16.6772 6.05065 16.9374 6.15122 17.1801C6.25178 17.4228 6.40017 17.6433 6.58597 17.829ZM11.384 5.478C11.8524 5.00875 12.4089 4.63663 13.0216 4.383C13.6342 4.12937 14.2909 3.99921 14.954 4C15.926 4 16.899 4.28 17.742 4.839C17.762 4.813 17.785 4.789 17.808 4.765C17.9043 4.66644 18.0197 4.58862 18.1472 4.53633C18.2747 4.48405 18.4116 4.45841 18.5493 4.46099C18.6871 4.46357 18.8229 4.49432 18.9484 4.55134C19.0738 4.60837 19.1863 4.69046 19.2788 4.79256C19.3713 4.89467 19.442 5.01463 19.4864 5.14507C19.5309 5.27551 19.5482 5.41366 19.5372 5.55103C19.5263 5.68839 19.4873 5.82206 19.4228 5.94382C19.3583 6.06558 19.2695 6.17284 19.162 6.259C19.8046 7.22976 20.0919 8.3928 19.9752 9.55112C19.8586 10.7094 19.3452 11.7918 18.522 12.615L17.797 13.34C17.015 14.123 15.984 15.55 15.485 16.547L13.976 19.563C13.727 20.063 13.203 20.147 12.805 19.75L4.24897 11.195C3.85197 10.798 3.94097 10.271 4.43597 10.023L7.45297 8.515C8.44197 8.021 9.87297 6.989 10.659 6.203L11.384 5.478ZM14.123 15.108C14.64 14.133 15.691 12.712 16.477 11.926L17.202 11.2C17.6984 10.7032 18.0081 10.0502 18.0786 9.35144C18.1491 8.65266 17.9762 7.95096 17.589 7.365C17.399 7.079 16.871 6.599 16.73 6.505C16.2308 6.17484 15.6454 5.99918 15.047 6C14.6466 5.99937 14.25 6.07785 13.88 6.23093C13.51 6.384 13.1739 6.60866 12.891 6.892L11.941 7.843C11.157 8.628 9.72197 9.663 8.73997 10.154L6.99997 11.024L13.07 17.093L14.123 15.108Z"
+                fill="#44546F"
+              />
+            </svg>
+          </Button>
+          <Button className="relative w-8 h-8 aspect-square rounded-full bg-transparent active:bg-primary-100 hover:bg-primary-100 md:block hidden cursor-pointer">
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute top-0 left-0"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M6 16C6 10.4767 10.4767 6 16 6C21.5233 6 26 10.4767 26 16C26 21.5233 21.5233 26 16 26C10.4767 26 6 21.5233 6 16ZM8 16C8 20.4188 11.5812 24 16 24C20.4188 24 24 20.4188 24 16C24 11.5812 20.4188 8 16 8C11.5812 8 8 11.5812 8 16ZM12 14C12 11.4838 14.3214 9.51108 16.9389 10.1071C18.3829 10.4351 19.5569 11.6051 19.8899 13.0481C20.3809 15.1771 19.1719 17.0911 17.3589 17.7471C17.1549 17.8201 17.0099 18.0021 17.0099 18.2191V18.0001C17.0099 18.5521 16.5629 19.0001 16.0099 19.0001C15.4579 19.0001 15.0099 18.5521 15.0099 18.0001V16.9871C15.0179 16.4411 15.4599 16.0001 15.9999 16.0001C17.1029 16.0001 17.9999 15.1021 17.9999 14.0001C17.9999 12.8971 17.1029 12.0001 15.9999 12.0001C14.8959 12.0001 13.9993 12.9231 14.0004 14.0271C13.9852 14.5666 13.5429 15 13 15C12.4777 15 12.0486 14.599 12.0038 14.0882C12.0038 14.0882 12 14.0297 12 14ZM16 22C15.448 22 15 21.552 15 21C15 20.448 15.448 20 16 20C16.552 20 17 20.448 17 21C17 21.552 16.552 22 16 22Z"
+                fill="#44546F"
+              />
+            </svg>
+          </Button>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <div className="rounded-full cursor-pointer">
@@ -227,21 +252,29 @@ function NavBar() {
                 <div className="mr-2 h-5 w-5">
                   <img src={Notification} className="mr-2 h-full w-full" />
                 </div>
-                <button className="button">Notification</button>
+                <Button className="p-0 font-normal h-auto" variant={"ghost"}>
+                  Notification
+                </Button>
               </DropdownMenuItem>
               <DropdownMenuItem className="flex md:hidden">
                 <div className="mr-2 h-5 w-5">
                   <img src={Information} className="mr-2 h-full w-full" />
                 </div>
-                <button className="button">Information</button>
+                <Button className="p-0 font-normal h-auto" variant={"ghost"}>
+                  Information
+                </Button>
               </DropdownMenuItem>
               <DropdownMenuItem onClick={openAccountSettings}>
-                <Settings className="mr-2 h-4 w-4" />
-                <button className="button">Account Settings</button>
+                <Settings className="mr-2 h-4 w-4 text-[#44546F]" />
+                <Button className="p-0 font-normal h-auto" variant={"ghost"}>
+                  Account Settings
+                </Button>
               </DropdownMenuItem>
               <DropdownMenuItem onClick={logout}>
-                <LogOut className="mr-2 h-4 w-4" />
-                <button className="button">Log out</button>
+                <LogOut className="mr-2 h-4 w-4 text-[#44546F]" />
+                <Button className="p-0 font-normal h-auto" variant={"ghost"}>
+                  Log out
+                </Button>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
