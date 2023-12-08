eee1672d06bdc12147d998967e4f15c5bb66020a
hitesh3121 <hitesh.fluxbyte@gmail.com>
2023-12-08 12:43:21 +0530

    backend userschema changes

--- packages/backend/src/schemas/userSchema.ts
+++ packages/backend/src/schemas/userSchema.ts
@@ -26,7 +26,7 @@ export const userOrgSettingsUpdateSchema = z.object({
 
 export const avatarImgSchema = z
   .any()
-  .refine((files) => files?.avatarImg?.size <= 500000, `Max file size is 5MB.`)
+  .refine((files) => files?.avatarImg?.size <= 1024 * 1024 * 5, {message:"Max file size is 5MB."})
   .refine(
     (files) =>
       ["image/jpeg", "image/jpg", "image/png", "image/webp"].includes(
