e5ddf18bd59d4c440bced5bb1a66f96df6e58c5d
priyasoni1709 <priya.fluxbyte@gmail.com>
2023-12-11 11:43:00 +0530

    Removed backdrop in loader

--- packages/frontend/src/components/common/Loader.tsx
+++ packages/frontend/src/components/common/Loader.tsx
@@ -3,7 +3,7 @@ import Spinner from "../ui/spinner";
 function Loader() {
   return (
     <div>
-      <div className="absolute w-full h-full grid place-content-center backdrop-blur-[0.5px] bg-[#7b797936] z-10 ">
+      <div className="absolute w-full h-full grid place-content-center  bg-[#7b797936] z-10 ">
         <Spinner color="#F99807" className="h-20 w-20" />
       </div>
     </div>
