be32d9480505ba2ea58c1ce6c56a5200203851b4
Sumit Patel <135584112+sumitFluxbyte@users.noreply.github.com>
2023-12-20 15:39:22 +0530

    unwanted change revert

--- packages/frontend/src/api/mutation/useSignupMutation.ts
+++ packages/frontend/src/api/mutation/useSignupMutation.ts
@@ -1,30 +1,37 @@
 import { useMutation } from "@tanstack/react-query";
 import { requestURLs } from "../../Environment";
-import {
-  AxiosResponseAndError,
-  ResponseType,
-} from "@/api/types/axiosResponseType";
-import ApiRequest from "../ApiRequest";
+import axios from "axios";
 import { z } from "zod";
-import { milestoneTaskSchema } from "@backend/src/schemas/taskSchema";
-import { Task } from "./useTaskCreateMutation";
+import { AxiosResponseAndError, ResponseType } from "@/api/types/axiosResponseType";
+import { authSignUpSchema } from "@backend/src/schemas/authSchema";
 
-type TaskAddUpdateMilestoneResponse = ResponseType<Task>;
+type SignupResponseType = ResponseType<{
+  token: string;
+  user: {
+    userId: string;
+    email: string;
+    status: string;
+    firstName: string | null;
+    lastName: string | null;
+    timezone: string | null;
+    country: string | null;
+    avatarImg: string | null;
+    createdAt: string;
+    updatedAt: string;
+  };
+}>;
 
-function useTaskAddUpdateMilestoneMutation(taskId?: string) {
+function useSignupMutation() {
   const mutation = useMutation<
-    AxiosResponseAndError<TaskAddUpdateMilestoneResponse>["response"],
-    AxiosResponseAndError<TaskAddUpdateMilestoneResponse>["error"],
-   z.infer<typeof milestoneTaskSchema>
+    AxiosResponseAndError<SignupResponseType>["response"],
+    AxiosResponseAndError<SignupResponseType>["error"],
+   z.infer<typeof authSignUpSchema>
   >({
     mutationFn: (data) =>
-      ApiRequest.post<TaskAddUpdateMilestoneResponse>(
-        `${requestURLs.task}milestone/${taskId}`,
-        data
-      ),
+      axios.post<SignupResponseType>(requestURLs.signup, data),
   });
 
   return mutation;
 }
 
-export default useTaskAddUpdateMilestoneMutation;
+export default useSignupMutation;
