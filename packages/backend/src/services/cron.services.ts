ac0c7e172b745099cfab8b2aaa1e6336c2f53ae3
hitesh3121 <hitesh.fluxbyte@gmail.com>
2024-01-12 11:46:48 +0530

    cron - changes

--- packages/backend/src/services/cron.services.ts
+++ packages/backend/src/services/cron.services.ts
@@ -4,14 +4,14 @@ import { EmailService } from "./email.services.js";
 import { NotificationTypeEnum } from "@prisma/client";
 
 export class CronService {
-  static async oneDayCron() {
+  static async oneMonthCron() {
     cron.schedule(
-      "0 0 * * *",
+      "0 0 1 * *",
       async () => {
         try {
-          await this.sendNotificationAndEmailToTaskDueDate();
+          console.log("called-this oneMonthCron");
         } catch (error) {
-          console.error("Error in oneDayCron:", error);
+          console.error("Error in oneMonthCron:", error);
         }
       },
       {
@@ -21,41 +21,54 @@ export class CronService {
   }
 
   static async sendNotificationAndEmailToTaskDueDate() {
-    const prisma = await getClientByTenantId("root");
-    const tasks = await prisma.task.findMany({
-      where: {
-        dueDate: {
-          equals: new Date(),
-        },
-      },
-      include: {
-        assignedUsers: {
-          include: {
-            user: true,
-          },
-        },
-      },
-    });
-    for (const task of tasks) {
-      const assignedUsers = task.assignedUsers.map((user) => user);
-      for (const user of assignedUsers) {
-        const email = user.user.email;
-        const userId = user.assginedToUserId;
-        const subjectMessage = `Task due today`;
-        const message = `Task '${task.taskName}' is due today.}`;
+    cron.schedule(
+      "0 0 * * *",
+      async () => {
+        try {
+          const prisma = await getClientByTenantId("root");
+          const tasks = await prisma.task.findMany({
+            where: {
+              dueDate: {
+                equals: new Date(),
+              },
+            },
+            include: {
+              assignedUsers: {
+                include: {
+                  user: true,
+                },
+              },
+            },
+          });
+          for (const task of tasks) {
+            const assignedUsers = task.assignedUsers.map((user) => user);
+            for (const user of assignedUsers) {
+              const email = user.user.email;
+              const userId = user.assginedToUserId;
+              const subjectMessage = `Task due today`;
+              const message = `Task '${task.taskName}' is due today.}`;
 
-        //Send Notification
-        await prisma.notification.sendNotification(
-          NotificationTypeEnum.TASK,
-          message,
-          userId,
-          userId,
-          task.taskId
-        );
+              //Send Notification
+              await prisma.notification.sendNotification(
+                NotificationTypeEnum.TASK,
+                message,
+                userId,
+                userId,
+                task.taskId
+              );
 
-        //Send Email
-        await EmailService.sendEmail(email, subjectMessage, message);
-      }
-    }
+              //Send Email
+              await EmailService.sendEmail(email, subjectMessage, message);
+            }
+          }
+        } catch (error) {
+          console.error(
+            "Error in sendNotificationAndEmailToTaskDueDate:",
+            error
+          );
+        }
+      },
+      { scheduled: true }
+    );
   }
 }
