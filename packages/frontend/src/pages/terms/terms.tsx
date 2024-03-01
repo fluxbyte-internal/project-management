8296bcc02612c30b91d526e06b352b20f35a510d
sumitFluxbyte <sumitpatel.fluxbyte@gmail.com>
2024-03-01 13:35:14 +0530

    lint error fix

--- packages/frontend/src/pages/terms/terms.tsx
+++ packages/frontend/src/pages/terms/terms.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
 function Terms() {
   return (
     <div className='h-full justify-center items-center'>
         terms & condition
     </div>
-  )
+  );
 }
 
-export default Terms
+export default Terms;
